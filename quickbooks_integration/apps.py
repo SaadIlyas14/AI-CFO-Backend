adf6a7248557a89ed5f32ec8294d5bd44d0fc2f2
Saad Ilyas <saad.ilyas@pentagonai.co>
2026-01-14 14:07:22 +0500

    express and node backend conversion

--- quickbooks_integration/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuickbooksIntegrationConfig(AppConfig):
-    name = 'quickbooks_integration'
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import SignupView, LoginView, PasswordResetRequestView, PasswordResetView
-
-urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('password-reset-request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
-    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
-]
